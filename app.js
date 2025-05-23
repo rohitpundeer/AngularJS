f34f12f3f296f96f492329a075248cfb5ee97e49
Rohit Pundeer <rohitpundeer@gmail.com>
2025-05-23 19:29:38 +0530

    Assignment 3 completed

--- app.js
+++ app.js
@@ -5,8 +5,12 @@
         .controller('TooMuchController', TooMuchController)
         .controller('ToBuyController', ToBuyController)
         .controller('AlreadyBoughtController', AlreadyBoughtController)
-        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+        .service('MenuSearchService', MenuSearchService)
+        .directive('foundItems', FoundItems);
 
+    /// All Controller Definations Goes Here   
     AppController.$inject = ["$scope"];
     function AppController($scope) {
         $scope.title = "Module 1 - Too Much";
@@ -30,19 +34,19 @@
         }
 
         $scope.btnReviewModule3Clicked = function () {
-            $scope.title = "Module 3 - Restaurant Menu";
+            $scope.title = "Module 3 - Menu Search";
             $scope.selectedModule = "M3";
             reviewModule();
         }
 
         $scope.btnReviewModule4Clicked = function () {
-            $scope.title = "Module 4 - Not Reacy Yet";
+            $scope.title = "Module 4 - Not Created Yet";
             $scope.selectedModule = "M4";
             reviewModule();
         }
 
         $scope.btnReviewModule5Clicked = function () {
-            $scope.title = "Module 5 - Not Reacy Yet";
+            $scope.title = "Module 5 - Not Created Yet";
             $scope.selectedModule = "M5";
             reviewModule();
         }
@@ -87,17 +91,53 @@
         }
     }
 
-    ToBuyController.$inject = ["$scope", "ShoppingListCheckOffService"];
-    function ToBuyController($scope, ShoppingListCheckOffService) {
-        $scope.toBuyList = ShoppingListCheckOffService.getToBuyList();
-        $scope.buyClicked = function (index) {
+    ToBuyController.$inject = ["ShoppingListCheckOffService"];
+    function ToBuyController(ShoppingListCheckOffService) {
+        this.toBuyList = ShoppingListCheckOffService.getToBuyList();
+        this.buyClicked = function (index) {
             ShoppingListCheckOffService.buyClicked(index);
         }
     }
-    AlreadyBoughtController.$inject = ["$scope", "ShoppingListCheckOffService"]
-    function AlreadyBoughtController($scope, ShoppingListCheckOffService) {
-        $scope.alreadyBoughtList = ShoppingListCheckOffService.getAlreadyBoughtList();
+
+    AlreadyBoughtController.$inject = ["ShoppingListCheckOffService"];
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+        this.alreadyBoughtList = ShoppingListCheckOffService.getAlreadyBoughtList();
+        this.reset = function () {
+            ShoppingListCheckOffService.resetLists();
+        }
     }
+
+    NarrowItDownController.$inject = ["MenuSearchService"];
+    function NarrowItDownController(MenuSearchService) {
+        var nidCtrl = this;
+        nidCtrl.searchText = "";
+        nidCtrl.message = "";
+        nidCtrl.foundItems = [];
+        nidCtrl.narrowItDownClicked = function () {
+            nidCtrl.message = "";
+            if (!nidCtrl.searchText?.trim()) {
+                nidCtrl.foundItems = [];
+                nidCtrl.message = "Nothing found!";
+                return;
+            }
+            var promise = MenuSearchService.getMatchedMenuItems(nidCtrl.searchText.trim());
+
+            promise.then(function (found) {
+                nidCtrl.foundItems = found;
+                if (nidCtrl.foundItems.length === 0) {
+                    nidCtrl.message = "Nothing found!";
+                }
+            }).catch(function (error) {
+                console.log("Error : " + error);
+                nidCtrl.message = "Nothing found!";
+            });
+        }
+        this.removeMenuItem = function (index) {
+            nidCtrl.foundItems.splice(index, 1);
+        }
+    }
+
+    /// Services Goes Here 
     function ShoppingListCheckOffService() {
         var toBuyList = [{ name: "Cookies", quantity: 10 },
         { name: "Soda", quantity: 5 },
@@ -119,6 +159,50 @@
             alreadyBoughtList.push(toBuyList[index]);
             toBuyList.splice(index, 1);
         }
+    }
 
+    MenuSearchService.$inject = ["$http", "$filter"];
+    function MenuSearchService($http, $filter) {
+
+        this.getMatchedMenuItems = function (searchTerm) {
+            return $http({
+                method: 'get',
+                url: 'https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json'
+            }).then(function (response) {
+                var data = [];
+                angular.forEach(response.data, function (category) {
+                    angular.forEach(category.menu_items, function (item) {
+                        if ((item.name.toLowerCase()).indexOf(searchTerm.toLowerCase()) > -1)
+                            data.push(item);
+                    });
+                });
+                return data;
+            });
+
+        }
+
+        function asyncGreet(name) {
+            // perform some asynchronous operation, resolve or reject the promise when appropriate.
+            return $q(function (resolve, reject) {
+                setTimeout(function () {
+                    if (okToGreet(name)) {
+                        resolve('Hello, ' + name + '!');
+                    } else {
+                        reject('Greeting ' + name + ' is not allowed.');
+                    }
+                }, 1000);
+            });
+        }
+    }
+
+    function FoundItems() {
+        var ddo = {
+            templateUrl: 'foundItems.html',
+            scope: {
+                items: '<',
+                onRemove: '&'
+            }
+        }
+        return ddo;
     }
 })();
